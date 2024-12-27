15a69f6885bc629bd5f0ee320829a3d6e35f932f
bayajeed <bayajeedhossain@gmail.com>
2024-12-28 00:26:20 +0600

    html page rander

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Home',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'Ecom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'Home/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
