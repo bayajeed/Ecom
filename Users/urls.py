a8024c733c06acbb7727d0a9ce19cbb0c398da36
bayajeed <bayajeedhossain@gmail.com>
2024-12-28 13:46:09 +0600

    same User app Create then Enviroment setup but template er vitor home a je name dichilam oi same name user er static files er name r template er name rakhar karone error dicchilo tai abar update dichi

--- Users/urls.py
+++ Users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserLogin
 
 urlpatterns = [
-    path('', UserLogin, name = 'UserLogin'),
+    path('users/', UserLogin, name = 'UserLogin')
 ]
--- Users/views.py
+++ Users/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def UserLogin(request):
-    return render(request, 'index.html', name = 'UserLogin')
+    return render(request, 'userlogin.html')
