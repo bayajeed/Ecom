443f3ba709d24ab68a4573857e6a84b1ce9ce1fc
bayajeed <bayajeedhossain@gmail.com>
2024-12-28 17:19:17 +0600

    User profile create then model, admin

--- Users/admin.py
+++ Users/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import CustomUser
 # Register your models here.
+admin.site.register(CustomUser) # Registering CustomUser model in admin panel, (na add korle admin panel e dekha jabe na)
