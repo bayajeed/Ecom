8864f91221e30d477ed2ebe622d64d6b31491fa2
bayajeed <bayajeedhossain@gmail.com>
2025-01-12 23:58:02 +0600

    `Updated cart functionality with JavaScript and Django views`

---
+++ Orders/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from .models import Cart,CartItem
+
+
+def CartView(request):
+    print('testtttttttttttttt',request.user)
+    if not request.user.is_authenticated:
+        return redirect('signin')
+    user = request.user
+    cart = Cart.objects.filter(user = user).first()
+    print(cart)
+    if not cart:
+        cart = Cart.objects.create(user = user)
+    cart_items = Cart.products.through.objects.filter(cart = cart)
+    contex = {
+        'cart_items': cart_items
+    }
+    return render(request, 'cart.html', contex)
+
